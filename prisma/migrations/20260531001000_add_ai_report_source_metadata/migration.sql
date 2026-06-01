41835ca31624c3121c5103fc8c07d5fb3fe90509
brockno11 <brockno11@github.com>
2026-06-02 07:36:46 +0800

    fix: 补充迁移文件遗漏的 sourceBatchId 列

--- prisma/migrations/20260531001000_add_ai_report_source_metadata/migration.sql
+++ prisma/migrations/20260531001000_add_ai_report_source_metadata/migration.sql
@@ -1,3 +1,4 @@
 ALTER TABLE "AIReport" ADD COLUMN "sourceRecordId" TEXT;
 ALTER TABLE "AIReport" ADD COLUMN "sourceRecordDate" DATETIME;
 ALTER TABLE "AIReport" ADD COLUMN "sourceSummary" TEXT;
+ALTER TABLE "AIReport" ADD COLUMN "sourceBatchId" TEXT;
